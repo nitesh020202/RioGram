713adce45d02d3ac3404307c2d7cc61c63b1d884
nitesh020202 <nitesh020202@gmail.com>
2025-08-26 16:36:51 +0530

    Riogram

--- src/App.js
+++ src/App.js
@@ -6,21 +6,21 @@ import AboutUs from './pages/About/about';
 import ContactUs from './pages/ContactUs/ContactUs';
 import MainContent from './pages/Home/home';
 import PrivacyPolicy from './pages/Privacy/privacy';
+
 function App() {
   return (
-    <Router basename="/RioGram">
+    <Router basename="/RioGram">   {/* 👈 important for GitHub Pages */}
       <Header />
       <Routes>
+        <Route path="/" element={<MainContent />} />   {/* ✅ default home */}
+        <Route path="/home" element={<MainContent />} />
         <Route path="/about" element={<AboutUs />} />
         <Route path="/contact" element={<ContactUs />} />
-        <Route path="/home" element={<MainContent />} />
-        <Route path="/RioGram" element={<MainContent />} />
         <Route path="/privacy" element={<PrivacyPolicy />} />
-        {/* Other routes */}
       </Routes>
-      {/* <Footer /> */}
     </Router>
   );
 }
 
 export default App;
+
